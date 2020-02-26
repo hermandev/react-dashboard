11bdaa5d212a56e7ad4276b560ae656079e2a76d
hermandev <hermantolakoro@gmail.com>
2020-02-26 16:09:34 +0800

    new update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './containers/layout/Dashboard'
+
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
